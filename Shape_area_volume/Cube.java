39d505b4769b03bbea65112c85520d8a83f450c8
Ayusha22-gho <65079025+Ayusha22-gho@users.noreply.github.com>
2021-08-29 23:22:02 +0530

    Update Cube.java

--- Shape_area_volume/Cube.java
+++ Shape_area_volume/Cube.java
@@ -6,12 +6,7 @@ public class Cube extends Shape implements Spatial
     private double width;
     private double height;
     
-//    public Cube(double length, double width, double height){
-//        this.length = length;
-//        this.width = width;
-//        this.height = height;
-//    }
-//    
+
     public double getLength(){
         return length;
     }
